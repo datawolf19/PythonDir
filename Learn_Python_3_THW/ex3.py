382b5b49f57e78920538b28d87b8334f499b9b4b
Wolfgang Morton <wolfgang.morton@gmail.com>
2019-03-23 16:27:56 -0700

    finished exercises 2 & 3.

---
+++ Learn_Python_3_THW/ex3.py
@@ -0,0 +1,34 @@
+print("I will now count my chickens:")
+
+# Uses print formatting to perform the operations within
+# the curly braces. 
+print("Hens {}".format(25+30/6))
+
+# Using f-strings to perform the calculation within the curly braces.
+print(f"Roosters {100-25 * 3 % 4}")
+
+# Printing a statement to the user.
+print("Now I will count the eggs:")
+
+# Performing these operations and printing to the user.
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Printing a statement to the user.
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Evaluating the truth of this statement. Returns boolean.
+print(3 + 2 < 5 - 7)
+
+# Print statement along with the calculation within curly braces.
+print(f"What is 3 + 2? {3 + 2}")
+print(f"What is 5 - 7? {5 - 7}")
+
+# Print statement to the user.
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+# These all evaluate the truthfulness of these statements. 
+print(f"Is it greater? {5 > -2}")
+print(f"Is it greater or equal? {5 >= -2}")
+print(f"Is it less or equal? {5 <= -2}")
