a1ade9529792faedd40a4ebe3803f2c0812b997b
Wolfgang Morton <wolfgang.morton@gmail.com>
2019-07-11 00:30:48 -0700

    initial commit

---
+++ Python - Object Oriented Programming/contact.py
@@ -0,0 +1,28 @@
+class ContactList(list):
+    def search(self, name):
+        """Return all contacts that contain the search value
+        in their name."""
+        matching_contacts = []
+        for contact in self:
+            if name in contact.name:
+                matching_contacts.append(contact)
+        return matching_contacts 
+
+class Contact:
+    # Class variable: means it is part
+    # of the class definition. You 
+    # access this class variable with
+    # Contact.all_contacts syntax. 
+    all_contacts = ContactList()
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)
+
+class Supplier(Contact):
+    def order(self, order):
+        print(
+            "If this were a real system we would send "
+            f"'{order}' order to '{self.name}''"
+        )
---
+++ Python - Object Oriented Programming/thelongestdict.py
@@ -0,0 +1,9 @@
+# Example for extending a built in object.
+
+class LongNameDict(dict):
+    def longest_key(self):
+        longest = None
+        for key in self:
+            if not longest or len(key) > len(longest):
+                longest = key
+        return longest 
